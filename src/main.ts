3cc3d0fb11c54e9d3abe9c523e8ea70862f9b766
Adriano hilario <adrianoferreira31@gmail.com>
2023-01-23 11:50:09 -0300

    chore: calling the modules in app.module

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
-import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { AuthModule } from './auth/auth.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AuthModule);
+  const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
