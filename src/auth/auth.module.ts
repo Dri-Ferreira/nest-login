92ba5c9383176166e0f105e105f2ed8cc47a757e
Adriano hilario <adrianoferreira31@gmail.com>
2023-01-24 14:02:33 -0300

    feat(auth): implement module localStrategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { PrismaService } from 'src/database/cliente';
+import { LocalStrategy } from './strategies/local.strategy';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService, PrismaService],
+  providers: [AuthService, LocalStrategy, PrismaService],
 })
 export class AuthModule {}
